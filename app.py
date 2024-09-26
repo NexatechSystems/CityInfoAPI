f24709231db6e199fb7127c21b8ae34d96c7da1c
mohammed hoque <ST10028317@vcconnect.edu.za>
2024-09-26 18:33:33 +0200

    fixes to requirements

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
@@ -77,5 +79,6 @@ def get_location_details(location_name):
     })
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
